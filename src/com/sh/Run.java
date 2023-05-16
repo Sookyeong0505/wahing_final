870c3da9d5de6a2cd767b25cd80eebd8fcf06461
전수경 <sookyeong0505@gmail.com>
2023-05-16 17:44:30 +0900

    FirstUpdate

--- src/com/sh/Run.java
+++ src/com/sh/Run.java
@@ -3,7 +3,7 @@ package com.sh;
 import com.sh.member.view.MemberMenu;
 
 public class Run {
-	
+	// 미니 프로젝트 수정 시작!
 	public static void main(String[] args) {
 		System.out.println("------ ❤❤ 우리 가게 정상영업 합니다~!! ❤❤------");
 		new MemberMenu().startMenu();
--- src/com/sh/member/model/dto/Member.java
+++ src/com/sh/member/model/dto/Member.java
@@ -1,6 +1,5 @@
 package com.sh.member.model.dto;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
